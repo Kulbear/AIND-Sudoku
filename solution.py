13b4071df9decbcf4248a25b660ac186e490d6a7
Kulbear <yangji0311@gmail.com>
2017-04-19 02:47:50 +0800

    Add 'Only Choice' strategy

--- solution.py
+++ solution.py
@@ -76,7 +76,22 @@ def eliminate(values):
     return values
 
 def only_choice(values):
-    pass
+    """Finalize all values that are the only choice for a unit.
+
+    Go through all the units, and whenever there is a unit with a value
+    that only fits in one box, assign the value to this box.
+
+    Args:
+        values(dict): Sudoku in dictionary form.
+    Returns:
+        Resulting Sudoku in dictionary form after filling in only choices.
+    """
+    for unit in unit_list:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                values[dplaces[0]] = digit
+    return values
 
 def reduce_puzzle(values):
     pass
